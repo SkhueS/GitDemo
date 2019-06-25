cb2857142332dd903b560d0c4d3f9343c53bee35
Santana <ssithole496@gmail.com>
2019-06-25 10:35:45 +0200

    this is my modified code

--- src/test/java/co/za/ssquared/stepDefinations/stepDefinition.java
+++ src/test/java/co/za/ssquared/stepDefinations/stepDefinition.java
@@ -15,6 +15,8 @@ public class stepDefinition {
     @Given("User is on NetBanking landing page")
     public void user_is_on_netbanking_landing_page() throws Throwable {
         System.out.println("\n navigated to login url.....");
+        System.out.println("\n navigated to login url1.....");
+        System.out.println("\n navigated to login url2.....");
     }
 
     @When("User login into application with username and password")
